d07c45251167e37cd6f9b458fd2993f11032e9c4
manujohnson1234 <manujohnson2002@gmail.com>
2024-01-07 15:52:39 +0530

    added crawlPage

--- crawl.js
+++ crawl.js
@@ -1,6 +1,33 @@
 const {JSDOM} = require('jsdom')
 
 
+async function crawlPage(currentURL){
+    console.log(`actively crawling: ${currentURL}`);
+
+    try{
+        const resp = await fetch(currentURL)
+
+        if(resp.status > 399){
+            console.log(`error in fetch with status code: ${resp.status} on page ${currentURL}`)
+            return;
+        }
+
+        const contentType = resp.headers.get("content-type")
+
+        if(!contentType.includes("text/html")){
+            console.log(`non html response, content type: ${contentType}`)
+            return
+        }
+
+        console.log(await resp.text())
+    }catch(err){
+        console.log(`error in fetch: ${err.message}, on page: ${currentURL}`);
+    }
+    
+
+}
+
+
 
 function getURLsFromHTML(htmlBody, baseURL){
     const urls = []
@@ -46,5 +73,6 @@ function noralizeURL(urlString){
 
 module.exports = {
     noralizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 };
